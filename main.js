a56f18adea4a5a47bb31d758b80bbcf31f8d6c87
Marvelle <marvelle.blake@gmail.com>
2021-07-31 15:28:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function multiply() {
+    var result1 = document.getElementById("Enter length of the side of Farm").value * document.getElementById("Enter length of the side of Farm").value;
+    document.getElementById("Area").value = result1;
+
+    var result2 = document.getElementById("Enter length of the side of Farm").value * 4;
+    document.getElementById("Perimeter").value = result2;
+}
